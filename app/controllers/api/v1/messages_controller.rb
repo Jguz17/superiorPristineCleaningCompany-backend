903d877a8f570d43bb3c3c5666fe6a31077e8ae9
jguz17 <johnathanguzman17@gmail.com>
2020-09-07 01:57:22 -0500

    added bookings controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+    skip_before_action :authorized
+
+    def index 
+        messages = Message.all
+
+        render json: messages
+    end
+
+    def show 
+        message = Message.find(params[:id])
+    end
+
+    def destroy
+        Message.find(params[:id]).destroy
+
+        render json: {message: 'success'}
+    end
 end

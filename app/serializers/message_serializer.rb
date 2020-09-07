d97ef29ad79312cd3be14492cb9b97efd68d496a
jguz17 <johnathanguzman17@gmail.com>
2020-09-07 03:52:20 -0500

    updates

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,3 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :user_id, :id, :name, :email, :phone, :content
+  attributes :user_id, :id, :name, :email, :phone, :content, :datetime
 end
